98dfa2c8736d5aa2f3c8977f09b623ba16c2d81f
g1umov <vladislavglumov30@gmail.com>
2022-10-24 21:34:52 +0400

    Create location presenter
    Done the following subtasks:
    - create location presenter declaration and implementation
    - create location view model that is typealias of String type

---
+++ Weather/Presenters/LocationPresenter.swift
@@ -0,0 +1,66 @@
+//
+//  LocationPresenter.swift
+//  Weather
+//
+//  Created by Vladislav on 24.10.22.
+//
+
+import Foundation
+
+protocol LocationPresenter {
+    func search(inputText: String?)
+    func selectLocation(byIndex index: Int)
+}
+
+protocol LocationPresenterDelegate: AnyObject {
+    func present(locations: [LocationViewModel])
+    func present(error: String)
+}
+
+class LocationPresenterImpl: LocationPresenter {
+    private let locationService: LocationService
+    private var locations = [Location]()
+    
+    weak var delegate: LocationPresenterDelegate?
+    var completionHandler: ((Location) -> Void)?
+    
+    init(locationService: LocationService) {
+        self.locationService = locationService
+    }
+    
+    func search(inputText: String?) {
+        guard let _inputText = inputText else { return }
+        guard !(_inputText.isEmpty) else { return }
+        
+        locationService.geocode(addressString: _inputText) { [weak self] result in
+            guard let _self = self else { return }
+            
+            switch result {
+            case .success(let locations):
+                _self.handleSuccess(locations: locations)
+            case .failure(let error):
+                _self.handleFailure(error: error)
+            }
+        }
+    }
+    
+    func selectLocation(byIndex index: Int) {
+        guard index <= (locations.count - 1) else { return }
+        
+        let location = locations[index]
+        completionHandler?(location)
+    }
+    
+    private func handleSuccess(locations: [Location]) {
+        self.locations = locations
+        
+        let viewModels = locations.map { "\($0.name), \($0.state), \($0.country)" }
+        delegate?.present(locations: viewModels)
+    }
+    
+    private func handleFailure(error: Error) {
+        delegate?.present(error: "No matching location found")
+        NSLog("Unable to forward geocode address, \(error)")
+    }
+    
+}
---
+++ Weather/Presenters/ViewModels/LocationViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  LocationViewModel.swift
+//  Weather
+//
+//  Created by Vladislav on 24.10.22.
+//
+
+import Foundation
+
+typealias LocationViewModel = String
