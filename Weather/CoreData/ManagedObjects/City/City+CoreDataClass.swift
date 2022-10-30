c3aecb37b282e813a2d46b669cd4feab17b3d27b
g1umov <vladislavglumov30@gmail.com>
2022-10-30 20:19:19 +0400

    Create data model in Core Data stack
    Created data model with City entity

---
+++ Weather/CoreData/ManagedObjects/City/City+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  City+CoreDataClass.swift
+//  Weather
+//
+//  Created by Vladislav on 30.10.22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(City)
+public class City: NSManagedObject {
+
+}
