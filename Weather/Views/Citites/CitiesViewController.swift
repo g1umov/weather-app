3a55c1f4900ca5992d03e1f11d5a60702dc678b4
g1umov <vladislavglumov30@gmail.com>
2022-10-31 12:39:34 +0400

    Create cities view
    Done the following subtasks:
    - create cities view controller
    - create city table view cell
    - create city cell view model

---
+++ Weather/Views/Citites/CitiesViewController.swift
@@ -0,0 +1,129 @@
+//
+//  CitiesViewController.swift
+//  Weather
+//
+//  Created by Vladislav on 30.10.22.
+//
+
+import UIKit
+
+final class CitiesViewController: UIViewController {
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.refreshControl = refreshControl
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 58
+        tableView.backgroundColor = .clear
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(CityCell.self)
+        return tableView
+    }()
+    
+    private lazy var errorLabel: UILabel = {
+        let label = UILabel()
+        label.isHidden = true
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .lightText
+        return label
+    }()
+    
+    private lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(updateForecasts), for: .valueChanged)
+        return refreshControl
+    }()
+    
+    private let presenter: CitiesPresenter
+    private var dataSource: [CityViewModel] = []
+    
+    init(presenter: CitiesPresenter) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupLayouts()
+        presenter.prepareCities()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .lightGray
+        view.addSubview(tableView)
+        view.addSubview(errorLabel)
+    }
+    
+    private func setupLayouts() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        errorLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            errorLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12),
+            errorLabel.leadingAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.leadingAnchor),
+            errorLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        ])
+    }
+    
+    @objc private func updateForecasts() {
+        presenter.updateForecasts()
+    }
+}
+
+extension CitiesViewController: CitiesPresenterDelegate {
+    func present(viewModels: [CityViewModel]) {
+        DispatchQueue.main.async {
+            self.dataSource = viewModels
+            self.tableView.reloadData()
+            self.errorLabel.isHidden = true
+        }
+    }
+    
+    func update(viewModels: [CityViewModel]) {
+        DispatchQueue.main.async {
+            self.dataSource = viewModels
+            self.tableView.reloadData()
+            self.refreshControl.endRefreshing()
+            self.errorLabel.isHidden = true
+        }
+    }
+    
+    func present(error: String) {
+        DispatchQueue.main.async {
+            self.errorLabel.isHidden = false
+            self.errorLabel.text = error
+        }
+    }
+}
+
+extension CitiesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: CityCell = tableView.dequeueReusableCell(for: indexPath)
+        let viewModel = dataSource[indexPath.item]
+        
+        cell.selectionStyle = .none
+        cell.setup(viewModel)
+        
+        return cell
+    }
+}
+
+extension CitiesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        presenter.selectCity(index: indexPath.item)
+    }
+}
---
+++ Weather/Views/Citites/CityCell.swift
@@ -0,0 +1,79 @@
+//
+//  CityCell.swift
+//  Weather
+//
+//  Created by Vladislav on 30.10.22.
+//
+
+import UIKit
+
+final class CityCell: UITableViewCell {
+    lazy var iconView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.tintColor = .white
+        return imageView
+    }()
+    
+    lazy var cityLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 22)
+        label.textColor = .lightText
+        return label
+    }()
+    
+    lazy var temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 36, weight: .medium)
+        label.textColor = .white
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+        setupLayouts()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup(_ viewModel: CityViewModel) {
+        iconView.image = UIImage(systemName: viewModel.weathercode)
+        cityLabel.text = viewModel.city
+        temperatureLabel.text = viewModel.temperature
+    }
+    
+    private func setupView() {
+        backgroundColor = .clear
+        contentView.backgroundColor = .clear
+        contentView.addSubview(iconView)
+        contentView.addSubview(cityLabel)
+        contentView.addSubview(temperatureLabel)
+    }
+    
+    private func setupLayouts() {
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+        cityLabel.translatesAutoresizingMaskIntoConstraints = false
+        temperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        cityLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        contentView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 12, leading: 12, bottom: 12, trailing: 18)
+        
+        NSLayoutConstraint.activate([
+            iconView.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+            iconView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            iconView.centerYAnchor.constraint(equalTo: contentView.layoutMarginsGuide.centerYAnchor),
+            iconView.widthAnchor.constraint(equalTo: iconView.heightAnchor),
+            
+            cityLabel.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+            cityLabel.leadingAnchor.constraint(equalTo: iconView.trailingAnchor, constant: 18),
+            cityLabel.centerYAnchor.constraint(equalTo: contentView.layoutMarginsGuide.centerYAnchor),
+            
+            temperatureLabel.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+            temperatureLabel.leadingAnchor.constraint(greaterThanOrEqualTo: cityLabel.trailingAnchor, constant: 12),
+            temperatureLabel.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+            temperatureLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+}
